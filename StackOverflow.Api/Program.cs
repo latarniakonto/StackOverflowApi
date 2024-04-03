ecc9214c0d723b378b195f13f9b0554b59dc8665
latarniakonto <wisniewskikq27@gmail.com>
2024-04-03 10:56:17 +0200

    Inject tag service

--- StackOverflow.Api/Program.cs
+++ StackOverflow.Api/Program.cs
@@ -5,6 +5,7 @@ using StackOverflow.Infrastructure;
 using StackOverflow.Infrastructure.Authentication;
 using StackOverflow.Infrastructure.Authorization;
 using StackOverflow.Infrastructure.Clients;
+using StackOverflow.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var client = new MongoClient("mongodb://localhost:27017");
@@ -14,6 +15,7 @@ var database = client.GetDatabase("StackOverflow");
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<ITagsClient, TagsClient>();
 builder.Services.AddSingleton<MongoDbContext>(options => new MongoDbContext(database));
+builder.Services.AddScoped<ITagService, TagService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddIdentity<TagUser, TagRole>().AddMongoDbStores<TagUser, TagRole, ObjectId>
