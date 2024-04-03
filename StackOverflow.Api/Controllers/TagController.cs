fd98fc8d4a124564e674f616aafa59b24aed2d37
latarniakonto <wisniewskikq27@gmail.com>
2024-04-03 10:56:26 +0200

    Add tag controller

--- StackOverflow.Api/Controllers/TagController.cs
+++ StackOverflow.Api/Controllers/TagController.cs
@@ -1,19 +1,51 @@
 using StackOverflow.Infrastructure.Clients;
 using StackOverflow.Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
+using StackOverflow.Api.Models;
+using System.Diagnostics;
 
 namespace StackOverflow.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class TagController : ControllerBase
+public class TagController : Controller
 {
     private readonly ITagService _tagService;
-    private readonly ITagsClient _tagsClient;
 
-    public TagController(ITagService tagService, ITagsClient tagsClient)
+    public TagController(ITagService tagService)
     {
         _tagService = tagService;
-        _tagsClient = tagsClient;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetTags(
+        int page = 1,
+        int pageSize = 10,
+        string nameSortOrder = "asc",
+        string weightSortOrder = "asc")
+    {
+        try
+        {
+            var tags = await _tagService.GetTagsAsync(page, pageSize, nameSortOrder, weightSortOrder);
+            return Ok(tags);
+        }
+        catch (Exception ex)
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, Message = ex.Message });
+        }
+    }
+
+    [HttpPost("update")]
+    public async Task<ActionResult> UpdateTags()
+    {
+        try
+        {
+            var tags = await _tagService.GetOrUpdateAllTagsAsync();
+            return Ok(tags);
+        }
+        catch (Exception ex)
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, Message = ex.Message });
+        }
     }
 }
