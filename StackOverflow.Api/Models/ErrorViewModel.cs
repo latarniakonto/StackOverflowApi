2a5aba6fd62373cb02d9ee008ff7f1e97387330a
latarniakonto <wisniewskikq27@gmail.com>
2024-04-03 10:48:36 +0200

    Add message to error view model

--- StackOverflow.Api/Models/ErrorViewModel.cs
+++ StackOverflow.Api/Models/ErrorViewModel.cs
@@ -5,4 +5,8 @@ public class ErrorViewModel
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public string? Message { get; set; }
+
+    public bool HasMessage => !string.IsNullOrEmpty(Message);
 }
